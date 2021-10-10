7f7b73266bbd6688e1cf3a4a2d081ff7df4098f8
WeronikaBL <weronika@datenexperten.com>
2021-10-10 11:17:53 +0200

    feat: add pseudocode

---
+++ 012_guess_number/main.py
@@ -0,0 +1,17 @@
+from art import logo
+
+# Print logo
+
+print(logo)
+
+# Welcome screen
+print("Welcome to the guessing game!")
+# Choose a random number between 1 - 100
+# Choose difficulty level
+# assign number of tries
+# ask for a guess
+# check if lower/higher/exact
+# if exact - won -> end game -> ask if plays again
+# decrease number of tries
+# repeat
+# if no more number of tries - > lost -> ask if plays againa
