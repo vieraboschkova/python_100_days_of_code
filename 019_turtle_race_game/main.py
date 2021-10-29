57bed00bae737133cfbca83a90749f40e17fd25f
WeronikaBL <weronika@datenexperten.com>
2021-10-29 19:08:46 +0200

    final

--- 019_turtle_race_game/main.py
+++ 019_turtle_race_game/main.py
@@ -1,7 +1,37 @@
-from turtle import Turtle, Screen, clear
+from turtle import Turtle, Screen
+import random
 
-# tim = Turtle()
+is_race_on = False
+colors = ["blue", "red", "yellow", "green", "purple"]
+turtles_y_coordinates = [100, 50, 0, -50, -100]
 screen = Screen()
+screen.setup(width=600, height=500)
+bet = screen.textinput(title="BET!", prompt="chooose a color").lower()
+print(bet)
+turtles = list()
+
+for position in range(0, len(colors)):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[position])
+    tim.penup()
+    tim.goto(x=-280,y=turtles_y_coordinates[position])
+    turtles.append(tim)
+
+if bet:
+    is_race_on = True
+
+while is_race_on:
+    for single_turtle in turtles:
+        distance_to_move = random.randint(1,20)
+        single_turtle.forward(distance_to_move)
+        if single_turtle.xcor() > 260:
+            print(f"!!!{single_turtle.pencolor()} won!!!")
+            if single_turtle.pencolor() == bet:
+                print("YOU WERE RIGHT!")
+            else:
+                print("SORRY, WROOOOOONG!")
+            is_race_on = False
+
 screen.listen()
 
 # def move_forward():
@@ -26,10 +56,10 @@ screen.listen()
 # screen.onkey(turn_right, 'd')
 # screen.onkey(clear, 'c')
 
-colors = []
-screen.setup(600,400)
-bet = screen.textinput("Choose the winner", "Which rainbow color turtle is going to win? Type a turtle color: ")
-print(bet)
+# colors = []
+# screen.setup(600,400)
+# bet = screen.textinput("Choose the winner", "Which rainbow color turtle is going to win? Type a turtle color: ")
+# print(bet)
 
 
 
